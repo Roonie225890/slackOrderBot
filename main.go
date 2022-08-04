d24c54c2eea5e1a6627da5693a1ce55d980f403a
Roonie <roonie@gapp.nthu.edu.tw>
2022-08-04 22:37:32 +0800

    feat: order meals

--- main.go
+++ main.go
@@ -11,40 +11,45 @@ import (
 	"github.com/shomali11/slacker"
 )
 
-// func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-// 	for event := range analyticsChannel {
-// 		fmt.Println("Command Events")
-// 		fmt.Println(event.Timestamp)
-// 		fmt.Println(event.Command)
-// 		fmt.Println(event.Parameters)
-// 		fmt.Println(event.Event)
-// 		fmt.Println()
-// 	}
-// }
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		// fmt.Println(event.Timestamp)
+		// fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		// fmt.Println(event.Event)
+		// fmt.Println()
+	}
+}
 
 type User struct {
 	Name  string
-	Meals []Meal
+	Meals map[string]Meal
 }
 
 type Meal struct {
 	Name  string
 	price int
+	amount int
 }
 
-var users []User
+var users = make(map[string]User)
 
 func main() {
-	
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-	// go printCommandEvents(bot.CommandEvents())
+	go printCommandEvents(bot.CommandEvents())
 
 	bot.Command("{operation} {price} {meal} {number} {userName}", &slacker.CommandDefinition{
 		Description: "+/- 餐  數量",
-		Examples:    []string{"+ 排骨飯 1"},
+		Examples:    []string{"+ $50 排骨飯 1"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			operation := request.Param("operation")
 			meal := request.Param("meal")
+			price, err := strconv.Atoi(request.Param("price"))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 			number, err := strconv.Atoi(request.Param("number"))
 			if err != nil {
 				fmt.Println(err)
@@ -52,36 +57,40 @@ func main() {
 			}
 
 			var newUser User
-			var newMeal Meal
+			
 
 			newUser.Name = request.Param("userName")
 			if "" == newUser.Name {
 				newUser.Name = botCtx.Event().UserName
 			}
+
+			newMeal := Meal {
+				Name: meal,
+				price: price,
+				amount: number,
+			}		
 			
-			newMeal.Name = meal
-			newMeal.price = number
-			newUser.Meals = []Meal{newMeal}
+			newUser.Meals = map[string]Meal{newMeal.Name: newMeal}
 
-			already := false
-
-			for i := range users {
-				if(users[i].Name == newUser.Name) {
-					users[i].Meals = append(users[i].Meals, newMeal)
-					already = true
-					break
+			user, existingUser := users[newUser.Name]
+			if existingUser {
+				meal, existingMeal := user.Meals[newMeal.Name]
+				if existingMeal {
+					meal.amount += number
+					user.Meals[newMeal.Name] = meal
+				}else{
+					user.Meals[newMeal.Name] = newMeal
+				}
+			}else{
+				users[newUser.Name] = User{
+					Name: newUser.Name,
+					Meals: map[string]Meal{
+						newMeal.Name: newMeal,
+					},
 				}
 			}
 
-			if !already {
-				users = append(users, newUser)
-			}
-
-			for i := range users {
-				fmt.Println(users[i])
-			}
-
-			r := fmt.Sprintf("%s %s %d %s", operation, meal, number, newUser.Name)
+			r := fmt.Sprintf("點餐成功! %s: %s %d %s", operation, meal, number, newUser.Name)
 
 			response.Reply(r)
 		},
